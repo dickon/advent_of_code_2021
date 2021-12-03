fdadbb8559a412515aaa4d105aa5e7797a0a843e
Dickon Reed <dickon.reed@oliverwyman.com>
2021-12-03 10:28:34 +0000

    refactor: pull out countbit

--- 3/code.py
+++ 3/code.py
@@ -18,13 +18,17 @@ def splitstrip(text):
 def invert(x):
     return '0' if x == '1' else '1'
 
+def countbit(textlist, field):
+    c = Counter()
+    for entry in textlist:
+        c[entry[field]] += 1
+    return c
+
 def derive(textlist):
     gamma = ''
     episolon = ''
     for i in range(len(textlist[0])):
-        c = Counter()
-        for entry in textlist:
-            c[entry[i]] += 1
+        c = countbit(textlist, i)
         popular = max(c, key=c.get)
         gamma += popular
         episolon += invert(popular)
@@ -36,9 +40,7 @@ def calc(totals):
 def derive_lifesupport(textlist, most=True):
     working = textlist
     for i in range(len(textlist[0])):
-        c = Counter()
-        for entry in working:
-            c[entry[i]] += 1
+        c = countbit(working, i)
         popular = '1' if c['1']>=c['0'] else '0'
         chosen = popular if most else invert(popular)
         working = [x for x in working if x[i] == chosen]
@@ -57,4 +59,6 @@ print(calc(derive(data)))
 
 print(derive_lifesupport(testdata, True), derive_lifesupport(testdata, False))
 print(derive_lifesupport(testdata, True), derive_lifesupport(testdata, False))
-print(derive_lifesupport(data, True) * derive_lifesupport(data, False))
+r = derive_lifesupport(data, True) * derive_lifesupport(data, False)
+assert r == 6677951
+print(r)
