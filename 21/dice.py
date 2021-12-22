c33be57c9254738cf0ddad7a3ed4e7ebdecfb8e4
Dickon Reed <dickon.reed@oliverwyman.com>
2021-12-22 10:42:12 +0000

    solve

--- 21/dice.py
+++ 21/dice.py
@@ -38,38 +38,43 @@ for r1 in range(1,4):
 pprint(outcomes)
 def start2(start=(4,8), goal=1000, verbose=False):
 
+    print()
+    print('playing with start', start, 'goal', goal)
     @cache
     def run2(score_t, pos_t, round):
         p = round % 2
         wins = [0, 0]
         
-        for r1 in range(1,4):
-            for r2 in range(1,4):                
-                for r3 in range(1,4):
-                    pos_t2 = replace(pos_t, p, (pos_t[p] + (r1+r2+r3))%10 )
-                    score_t2 = replace(score_t, p, score_t[p]+pos_t2[p]+1)
-                    if verbose:
-                        print('after round',round, 'of player', p,'from scores', score_t, 'rolling',r1,r2,r3,'scores', score_t2, 'pos', pos_t2)
-                    if score_t2[p] >= goal:
-                        if verbose:
-                            print('player', p, 'wins!')
-                        wins[p] += 1
-                    else:
-                        assert score_t2[0] < goal
-                        assert score_t2[1] < goal
-                        if verbose:
-                            print('recursing with', score_t2, pos_t2,'to run round', round+1)
-                        nwins = run2(score_t2, pos_t2, round+1)
-                        wins[0] += nwins[0]
-                        wins[1] += nwins[1]
-        if round == 0 or verbose:
-            print('results after round',round,'is', wins )
+        for tot, count in outcomes.items():
+            pos_t2 = replace(pos_t, p, (pos_t[p] + tot)%10 )
+            score_t2 = replace(score_t, p, score_t[p]+pos_t2[p]+1)
+            if verbose:
+                print('after turn', p, 'from scores', score_t, 'rolling',tot,'scores', score_t2, 'pos', pos_t2)
+            if score_t2[p] >= goal:
+                if verbose:
+                    print('player', p, 'wins!')
+                wins[p] += count
+            else:
+                assert score_t2[0] < goal
+                assert score_t2[1] < goal
+                if verbose:
+                    print('recursing with', score_t2, pos_t2,'to run round', round+1)
+                nwins = run2(score_t2, pos_t2, (round+1)%2)
+                wins[0] += nwins[0] * count
+                wins[1] += nwins[1] * count
+        if verbose:
+            print('from scores', score_t, 'at pos', pos_t, 'wins are', wins)
         return wins
 
-    return run2( (0,0), start,  0)
+    start_pos = ( (start[0]-1, start[1]-1))
+    return run2( (0,0), start_pos,  0)
 
-assert start2(goal=3, verbose=True) == [365, 338]
+assert start2(goal=3, verbose=True) == [990, 207]
 assert start2(start=(0,0), goal=3, verbose=True) == [27, 0]
-print(start2())
+for pos in [(4,8), (7,5)]:
+    r = start2(start=pos, goal=21, verbose=False)
+    print(pos, r)
+    if pos == (4,8):
+        assert r == [444356092776315,341960390180808]
 
             
